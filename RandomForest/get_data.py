0e8d238ec9a69dd5445aed18f322cbef19eabf5b
Stefanie Kobsar Rodríguez <stkobsar7@gmail.com>
2021-03-09 23:32:49 +0100

    get data functionality

--- RandomForest/get_data.py
+++ RandomForest/get_data.py
@@ -1,1 +1,21 @@
 from sklearn.datasets import load_breast_cancer
+import pandas as pd
+import numpy as np
+
+def get_dataframe(cancer):
+    df_cancer = pd.DataFrame(np.c_[cancer['data'], cancer['target']],
+                      columns= np.append(cancer['feature_names'], ['target']))
+    return df_cancer
+
+def get_info_dataframe(df):
+    with open('dataset_info.txt','w') as out:
+      df.info(buf = out)
+
+if __name__ == "__main__":
+
+    working_df = get_dataframe(load_breast_cancer())
+    print(working_df.head())
+
+    get_info_dataframe(working_df)
+
+
