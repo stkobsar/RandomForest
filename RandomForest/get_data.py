e66ce11818dfa9049b2d257c469e2439151a0fa9
Stefanie Kobsar Rodríguez <stkobsar7@gmail.com>
2021-03-09 23:52:19 +0100

    function description

--- RandomForest/get_data.py
+++ RandomForest/get_data.py
@@ -3,19 +3,23 @@ import pandas as pd
 import numpy as np
 
 def get_dataframe(cancer):
+    """
+    Description: Get breast cancer data from sklearn datasets
+    :param cancer: sklearn dataset
+    :return: pandas dataframe
+    """
     df_cancer = pd.DataFrame(np.c_[cancer['data'], cancer['target']],
                       columns= np.append(cancer['feature_names'], ['target']))
     return df_cancer
 
 def get_info_dataframe(df):
+    """
+    Returns information of the datafram in a txt file
+    :param df: breast cancer dataframe
+    :return: dataset_info.txt file
+    """
     with open('dataset_info.txt','w') as out:
       df.info(buf = out)
 
-if __name__ == "__main__":
-
-    working_df = get_dataframe(load_breast_cancer())
-    print(working_df.head())
-
-    get_info_dataframe(working_df)
 
 
