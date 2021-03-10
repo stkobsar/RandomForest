22d6a2718aeb97532c09c2f098a64417ee7b6b88
Stefanie Kobsar Rodríguez <stkobsar7@gmail.com>
2021-03-10 20:40:33 +0100

    correlation matrix functionality

--- RandomForest/EDA.py
+++ RandomForest/EDA.py
@@ -43,22 +43,13 @@ def get_qqplot(df):
             print('Not able to return qqplot')
 
 def correlation_matrix(df):
-    # Compute the correlation matrix
     corr = df.corr()
-
     # Generate a mask for the upper triangle
     mask = np.triu(np.ones_like(corr, dtype=bool))
 
-    # Set up the matplotlib figure
     f, ax = plt.subplots(figsize=(11, 9))
-
-    # Generate a custom diverging colormap
-    #cmap = sns.diverging_palette(230, 20, as_cmap=True)
-
-    # Draw the heatmap with the mask and correct aspect ratio
     heatmap = sns.heatmap(corr, mask=mask, vmax=.3, center=0,
                 square=True, linewidths=.5, cbar_kws={"shrink": .5})
-    #Save fig
 
     fig = heatmap.get_figure()
     fig.savefig(os.path.join(path, "heatmap_breastcancer.png"))
