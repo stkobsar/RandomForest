ee1164bac7c8d66d71b4411ca5eb45cecf8bca9c
Stefanie Kobsar Rodríguez <stkobsar7@gmail.com>
2021-03-10 13:12:25 +0100

    Implement EDA file functionalities

--- RandomForest/main.py
+++ RandomForest/main.py
@@ -1,9 +1,16 @@
 from sklearn.datasets import load_breast_cancer
 import RandomForest.get_data as gd
+import RandomForest.EDA as EDA
 
 def main():
     working_df = gd.get_dataframe(load_breast_cancer())
-    print(working_df.head())
+    gd.get_info_dataframe(working_df)
+
+
+    EDA.get_histograms(working_df)
+    EDA.get_qqplot(working_df)
+    EDA.correlation_matrix(working_df)
+
 
 
 if __name__ == "__main__":
